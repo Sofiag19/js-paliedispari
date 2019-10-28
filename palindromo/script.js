e74fd6a60c3bcd218ddb3a1439f6cce598ebef69
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-10-28 14:19:18 +0100

    tutto lowercase

--- palindromo/script.js
+++ palindromo/script.js
@@ -4,13 +4,12 @@
 // devo permettere all'utente di inserire una parola
 
 var word = prompt("Inserisci una parola per scoprire se è palindroma");
-
+var wordLow = word.toLowerCase();
 // devo convertire la parola in un array, ogni carattere diventa un elemento
 var wordArray = [];
-for (var i = 0; i < word.length; i++) {
-  wordArray.push(word[i]);
+for (var i = 0; i < wordLow.length; i++) {
+  wordArray.push(wordLow[i]);
   console.log(wordArray);
-
 }
 
 // devo eliminare spazi e caratteri speciali, ed uniformare le lettere (o tutto maiuscolo o tutto minuscolo)
