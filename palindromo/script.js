8c7ec128157d7e439a69eb00a61aebaf8f9795ad
Sofiag19 <sofia.giacometti.19@gmail.com>
2019-10-28 14:31:08 +0100

    mi ci sto avvicinando... quasi

--- palindromo/script.js
+++ palindromo/script.js
@@ -2,15 +2,34 @@
 
 
 // devo permettere all'utente di inserire una parola
-
 var word = prompt("Inserisci una parola per scoprire se è palindroma");
+
+// uniformare i caratteri
 var wordLow = word.toLowerCase();
+
+// devo eliminare spazi e caratteri speciali (?????)
+
 // devo convertire la parola in un array, ogni carattere diventa un elemento
 var wordArray = [];
 for (var i = 0; i < wordLow.length; i++) {
   wordArray.push(wordLow[i]);
-  console.log(wordArray);
+}
+console.log(wordArray);
+
+// devo ciclare l'array confrontando la prima parola con l'ultima...la seconda con la penultima e così via
+var palindroma = false;
+for (j = 0 , k = wordArray.length - 1; j < wordArray.length ; j++, k-- ) {
+  if (wordArray[j] === wordArray[k]) {
+    console.log("parola palindroma");
+    // palindroma = true;
+  }else {
+    // palindroma = false;
+    console.log("non palindroma");
+  }
 }
 
-// devo eliminare spazi e caratteri speciali, ed uniformare le lettere (o tutto maiuscolo o tutto minuscolo)
-// devo ciclare l'array confrontando la prima parola con l'ultima...la seconda con la penultima e così via
+  // if (palindroma = true) {
+  // console.log("parola palindroma");
+  // } else {
+  //   console.log("non palindroma");
+  // }
